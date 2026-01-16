33888b21079946f53492c68152865a72eb9259f6
Niffy-the-conqueror <117080364+Niffy-the-conqueror@users.noreply.github.com>
2026-01-16 10:03:06 +0100

    fix: corected elements in the list view

--- static/src/js/WMDs_Component_Project/src/components/Forms/PickComponent.vue
+++ static/src/js/WMDs_Component_Project/src/components/Forms/PickComponent.vue
@@ -21,9 +21,8 @@
                             :invalid="!form_data[field]"
                             class="w-full" 
                             optionLabel="name"
-                            optionValue="name"
+                            optionValue="code"
                             />
-                    <label :for="field">{{ map_cols.filter(col => col.field === field)[0].name }}</label>
                 </FloatLabel>
             </div>
             <div v-if="extra_data">
