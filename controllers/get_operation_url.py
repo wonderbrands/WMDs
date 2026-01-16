56cb8a548ba7a9bd2229481f7a45ce68ac7f8f3e
Niffy-the-conqueror <117080364+Niffy-the-conqueror@users.noreply.github.com>
2026-01-16 13:04:48 +0100

    fix: corected elements in the list view

--- controllers/get_operation_url.py
+++ controllers/get_operation_url.py
@@ -14,7 +14,7 @@ logger = logging.getLogger(__name__)
 class GetURLOfPick(http.Controller):
 
     @http.route(
-        '/wmds/v2/engine/get/pick-url',
+        '/wmds/v2/engine/get/barcode-url',
         type='json',
         auth='user',
         methods=['POST'],
