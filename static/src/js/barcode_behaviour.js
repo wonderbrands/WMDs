2fba775f9d53573dbea5ffa511b3bdab4e3f4ef4
Niffy-the-conqueror <117080364+Niffy-the-conqueror@users.noreply.github.com>
2026-01-26 19:00:09 +0100

    added role log for other roles

--- static/src/js/barcode_behaviour.js
+++ static/src/js/barcode_behaviour.js
@@ -1,6 +1,6 @@
 /** @odoo-module **/
 
-import { BarcodeModel } from "@stock_barcode/models/barcode_model";
+import  BarcodeModel  from "@stock_barcode/models/barcode_model";
 import { patch } from "@web/core/utils/patch";
 
 patch(BarcodeModel.prototype, {
